0dedb940f937d8d3c3b377f535a087d9e626d101
Thomas--S <Thomas--S@users.noreply.github.com>
2021-08-14 19:09:21 +0200

    Compress textures

---
+++ compress_textures.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd ./ts_vehicles_api/textures
+pngquant --skip-if-larger --quality=100 --strip *.png --ext .png --force
+
+cd ../../ts_vehicles_common/textures
+pngquant --skip-if-larger --quality=100 --strip *.png --ext .png --force
+
+cd ../../ts_vehicles_cars/textures
+pngquant --skip-if-larger --quality=100 --strip *.png --ext .png --force
