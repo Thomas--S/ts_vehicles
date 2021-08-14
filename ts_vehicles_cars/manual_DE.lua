88cc78b2dd9f359a0c6ace2c61172045d93b4edd
Thomas--S <Thomas--S@users.noreply.github.com>
2021-08-14 19:00:22 +0200

    Add documentation

---
+++ ts_vehicles_cars/manual_DE.lua
@@ -0,0 +1,76 @@
+techage.add_to_manual('DE', {
+  "1,Fahrzeuge: Autos",
+  "2,Herstellung",
+  "2,Steuerung",
+  "3,Licht",
+  "2,Kofferraum",
+  "2,Treibstoff",
+  "3,Tankstellen",
+  "3,Reservekanister",
+  "2,Passagiere",
+}, {
+  "Autos können verwendet werden\\, um Spieler und Waren auf ebenem Terrain über lange Strecken zu befördern.\n"..
+  "\n",
+  "Zunächst muss ein \"Car Construction Stand\" platziert werden.\n"..
+  "Anschließend kannst du per Rechtsklick Teile zum Fahrzeug hinzufügen.\n"..
+  "\n"..
+  "Wenn du probierst\\, mit Rechtsklick in das Auto einzusteigen\\, erscheint eine Chat-Nachricht mit Informationen darüber\\, welche Teile noch fehlen.\n"..
+  "\n"..
+  "Das Formular des Autos kann mittels Shift + Rechtsklick geöffnet werden.\n"..
+  "\n"..
+  "Viele Teile (z.B. Chassis\\, Sitze\\, Innenausstattung etc.) können im Crafting-Feld eingefärbt werden.\n"..
+  "\n"..
+  "Zum Abbauen müssen der Kofferraum und der Tank entleert werden. Dann können alle Teile ausgebaut werden und der übrig bleibende Car Construction Stand mit Linksklick abgebaut werden.\n"..
+  "\n",
+  "Mit Rechtsklick kann man ein- und aussteigen.\n"..
+  "Das Fahrzeug wird mit den WASD-Tasten gesteuert.\n"..
+  "Zum Bremsen die Leertaste drücken.\n"..
+  "\n",
+  "Die Leuchteinrichtungen können mit folgenden Tastenkombinationen ein- und ausgeschaltet werden:\n"..
+  "\n"..
+  "Shift + W: Frontlicht\n"..
+  "Shift + A: Blinker (links)\n"..
+  "Shift + D: Blinker (rechts)\n"..
+  "Shift + A + D: Warnblinkanlage\n"..
+  "Shift + W + S: Sondersignal (falls vorhanden)\n"..
+  "\n"..
+  "Die Rückfahrscheinwerfer schalten sich automatisch ein\\, sobald man rückwärts fährt.\n"..
+  "\n",
+  "Wenn ein Linksklick auf das Auto mit einem Gegenstand erfolgt\\, wird dieser schnell und einfach in den Kofferraum gelegt.\n"..
+  "Der Kofferraum ist über das Formular des Autos (Shift + Rechtsklick zum Öffnen) erreichbar.\n"..
+  "\n",
+  "Autos können Benzin\\, Wasserstoff oder Elektrizität als Energiequellen verwenden.\n"..
+  "\n",
+  "Tankstellen müssen von unten an das entsprechende Netz (Benzin\\, Wasserstoff oder Strom) angeschlossen werden. Bei Benzin und Wasserstoff ist eine Pumpe nötig.\n"..
+  "\n"..
+  "Zum Tanken muss das Auto so platziert sein\\, dass zwischen Tankdeckel (Beifahrerseite hinten) und Zapfsäule kein Hindernis ist und die Entfernung weniger als 3 Meter beträgt.\n"..
+  "Im Formular der Zapfsäule kann dann das gewünschte Fahrzeug ausgewählt werden.\n"..
+  "\n",
+  "Wenn keine Tankstelle erreichbar ist\\, kann man das Auto auch über folgende Reservekanister betanken: Benzinkanister\\, Benzinfass\\, Wasserstoffflasche und TA3 Akkubox.\n"..
+  "\n",
+  "Neben dem Fahrer können bis zu drei weitere Spieler in einem Auto mitfahren.\n"..
+  "Einfach nach dem Fahrer einsteigen.\n"..
+  "\n"..
+  "Man kann auswählen\\, ob jeder als Passagier einsteigen kann oder nur Besitzer mitfahren dürfen.\n"..
+  "\n",
+}, {
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+}, {
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+})
